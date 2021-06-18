8c83684da8398f6200f6751079cf94153ae539d7
Jessica Watts <jessperr182@hotmail.com>
2021-06-18 04:00:17 +0000

    Automatically backed up by Learn

---
+++ parameters.rb
@@ -0,0 +1,9 @@
+def greeting
+  puts "Hi, Ruby programmer!"
+end
+// ♥ irb
+2.2.1 :001 > def greeting
+2.2.1 :002?>   puts "Hi, Ruby programmer!"
+2.2.1 :003?>   end
+2.2.1 :004 > greeting
+ => :greeting
